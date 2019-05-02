2d95d2f5dc62c16de814c3875edb3704d0fc3a77
tomocy <tomocy.dev@gmail.com>
2019-05-02 23:01:21 +0900

    add Delete to Sensei

--- sensei.go
+++ sensei.go
@@ -38,6 +38,12 @@ func (s *Sensei) Get(r *http.Request, key interface{}) (interface{}, error) {
 	return sess.Values[key], nil
 }
 
+func (s *Sensei) Delete(w http.ResponseWriter, r *http.Request) error {
+	sess, _ := s.Store.New(r, s.Key)
+	sess.Options.MaxAge = -1
+	return sess.Save(r, w)
+}
+
 func (s *Sensei) SetFlash(w http.ResponseWriter, r *http.Request, key string, value interface{}) error {
 	sess, err := s.Session(r)
 	if err != nil {
